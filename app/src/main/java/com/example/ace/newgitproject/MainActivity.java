83f8ee8c536cb4e576e16704921df8d8684dfcec
aleksandarvel <avelinov2@gmail.com>
2015-12-23 11:08:36 +0100

    3 commit!

--- app/src/main/java/com/example/ace/newgitproject/MainActivity.java
+++ app/src/main/java/com/example/ace/newgitproject/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ace.newgitproject;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b1=new Button(getBaseContext());
+
+        b1.setText("Hello world");
     }
 }
